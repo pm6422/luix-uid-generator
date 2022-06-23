e578d28bbfdba2b946332545c6aff156ae544534
刘尧 <liuyao@xforceplus.com>
2022-06-23 10:27:35 +0800

    add spring filter

--- luix-uid-generator-usage-demo/src/main/java/com/luixtech/uidgenerator/usage/demo/controller/HomeController.java
+++ luix-uid-generator-usage-demo/src/main/java/com/luixtech/uidgenerator/usage/demo/controller/HomeController.java
@@ -1,8 +1,13 @@
 package com.luixtech.uidgenerator.usage.demo.controller;
 
 import com.luixtech.uidgenerator.core.uid.UidGenerator;
+import com.luixtech.uidgenerator.usage.demo.domain.IdGeneratorWorkerNode;
 import com.luixtech.uidgenerator.usage.demo.repository.IdGeneratorWorkerNodeRepository;
+import com.turkraft.springfilter.boot.Filter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +31,15 @@ public class HomeController {
         String str = uidGenerator.parseUid(uid);
         log.info("Parsed uid: {}", str);
         return uid;
+    }
+
+    /**
+     * https://github.com/turkraft/spring-filter
+     * <p>
+     * /api/search?filter=id:1 and appId:'luix-uid-generator-usage-demo'
+     */
+    @GetMapping(value = "/api/search")
+    public Page<IdGeneratorWorkerNode> search(@Filter Specification<IdGeneratorWorkerNode> spec, Pageable page) {
+        return idGeneratorWorkerNodeRepository.findAll(spec, page);
     }
 }
